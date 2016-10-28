2383fa7d9d9d82edbe605697e9c4228a6a3f7898
kanade <pye52@163.com>
2016-10-28 09:08:22 +0800

    漏了两个类忘了添加

---
+++ jlog/src/main/java/com/jiongbull/jlog/CrashHandler.java
@@ -0,0 +1,63 @@
+package com.jiongbull.jlog;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+
+/**
+ * 自定义的崩溃捕获Handler
+ * Created by wenmingvs on 2016/7/4.
+ */
+public class CrashHandler implements Thread.UncaughtExceptionHandler {
+
+    private static final String TAG = "CrashHandler";
+    private static CrashHandler INSTANCE = new CrashHandler();
+
+    /**
+     * 保证只有一个CrashHandler实例
+     */
+    private CrashHandler() {
+    }
+
+    /**
+     * 获取CrashHandler实例 ,单例模式
+     */
+    public static CrashHandler getInstance() {
+        return INSTANCE;
+    }
+
+    /**
+     * 初始化,，设置此CrashHandler来响应崩溃事件
+     */
+    public void init() {
+        Thread.setDefaultUncaughtExceptionHandler(this);
+    }
+
+
+    /**
+     * 当UncaughtException发生时会转入该函数来处理
+     */
+    @Override
+    public void uncaughtException(final Thread thread, final Throwable ex) {
+        Writer writer = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(writer);
+        ex.printStackTrace(printWriter);
+        Throwable cause = ex.getCause();
+        while (cause != null) {
+            cause.printStackTrace(printWriter);
+            cause = cause.getCause();
+        }
+        printWriter.close();
+        String stringBuilder = "↓↓↓↓exception↓↓↓↓\n" +
+                writer.toString();
+        JLog.crash(stringBuilder);
+        // 如果处理了，让主程序继续运行3秒再退出，保证异步的写操作能及时完成
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
